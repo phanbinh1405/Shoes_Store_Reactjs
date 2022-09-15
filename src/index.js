7c74eb9681745cd8669fd4fee3794da834cb14fd
binhphan <binhphan@battlefi.io>
2022-09-15 20:57:16 +0700

    000

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 // Routing
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Store
+import { Provider } from 'react-redux';
+import { store } from './redux/configStore';
+
 import App from './App';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
@@ -13,17 +17,19 @@ import Carts from './pages/Carts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='' element={<App />}>
-          <Route index element={<Home/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='profile' element={<Profile/>}/>
-          <Route path='login' element={<Login/>} />
-          <Route path='search' element={<Search/>} />
-          <Route path='carts' element={<Carts/>} />
-        </Route>  
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<App />}>
+            <Route index element={<Home/>}/>
+            <Route path='register' element={<Register/>}/>
+            <Route path='profile' element={<Profile/>}/>
+            <Route path='login' element={<Login/>} />
+            <Route path='search' element={<Search/>} />
+            <Route path='carts' element={<Carts/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
 );
 
